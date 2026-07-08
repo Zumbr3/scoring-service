ee0f9f1fdec55af2de895d193006f128e8719954
Yuri Martinelli <martinelliyuri7@gmail.com>
2026-07-08 09:32:07 -0300

    feat: Add Gradle project

---
+++ src/main/java/com/yuri/estudo/scoringservice/ScoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.estudo.scoringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoringServiceApplication.class, args);
+	}
+
+}
