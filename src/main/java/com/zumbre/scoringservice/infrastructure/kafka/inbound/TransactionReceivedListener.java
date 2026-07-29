0b51e21f9a91efe66c37e1206612a04a4cc8b417
Yuri Martinelli <martinelliyuri7@gmail.com>
2026-07-29 13:16:30 -0300

    feat: publish transaction.approved verdict (walking skeleton 3/3)

--- src/main/java/com/zumbre/scoringservice/infrastructure/kafka/inbound/TransactionReceivedListener.java
+++ src/main/java/com/zumbre/scoringservice/infrastructure/kafka/inbound/TransactionReceivedListener.java
@@ -1,32 +1,43 @@
 package com.zumbre.scoringservice.infrastructure.kafka.inbound;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zumbre.scoringservice.infrastructure.kafka.outbound.ApprovedData;
+import com.zumbre.scoringservice.infrastructure.kafka.outbound.TransactionApprovedPublisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Component
 public class TransactionReceivedListener {
   private static final Logger logger = LoggerFactory.getLogger(TransactionReceivedListener.class);
   private final ObjectMapper objectMapper;
+  private final TransactionApprovedPublisher publisher;
 
-  public TransactionReceivedListener(ObjectMapper objectMapper){
+  public TransactionReceivedListener(ObjectMapper objectMapper, TransactionApprovedPublisher publisher) {
     this.objectMapper = objectMapper;
+    this.publisher = publisher;
   }
 
   @KafkaListener(topics = "${scoring.topics.received}")
   void onTransactionReceived(String payload) {
-    try{
-      TransactionReceivedEvent evento = objectMapper.readValue(payload, TransactionReceivedEvent.class);
-      logger.info("Json recebido: {}", evento);
-    }
-    catch (JsonProcessingException exception){
-      //TODO Fatia 6, implement DLQ
+    try {
+      TransactionReceivedEvent event =
+          objectMapper.readValue(payload, TransactionReceivedEvent.class);
+      logger.info("Json recebido: {}", event);
+
+      ApprovedData simuledTransaction =
+          new ApprovedData(event.data().transactionId(), new BigDecimal(15));
+
+      this.publisher.sendApprovedTransaction(simuledTransaction, event.data().accountId());
+
+    } catch (JsonProcessingException exception) {
+      // TODO Fatia 6, implement DLQ
       logger.error("payload inválido", exception);
     }
-
   }
 }
--- src/main/java/com/zumbre/scoringservice/infrastructure/kafka/outbound/TransactionApprovedPublisher.java
+++ src/main/java/com/zumbre/scoringservice/infrastructure/kafka/outbound/TransactionApprovedPublisher.java
@@ -1,3 +1,41 @@
 package com.zumbre.scoringservice.infrastructure.kafka.outbound;
 
-public class TransactionApprovedPublisher {}
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zumbre.scoringservice.infrastructure.kafka.inbound.TransactionReceivedListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class TransactionApprovedPublisher {
+  private final ObjectMapper objectMapper;
+  private final KafkaTemplate<String, String> kafkaTemplate;
+  @Value("${scoring.topics.approved}") String approvedTopic;
+
+  public TransactionApprovedPublisher(
+      ObjectMapper objectMapper, KafkaTemplate<String, String> kafkaTemplate) {
+    this.objectMapper = objectMapper;
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void sendApprovedTransaction(ApprovedData transactionApproved, UUID accountId) throws JsonProcessingException {
+    TransactionApprovedEvent event =
+        new TransactionApprovedEvent(
+            UUID.randomUUID(),
+            "transaction.approved",
+            1,
+            Instant.now(),
+            "scoring-service",
+            transactionApproved);
+
+    String stringEvent = objectMapper.writeValueAsString(event);
+
+    this.kafkaTemplate.send(approvedTopic, accountId.toString(), stringEvent);
+  }
+}
